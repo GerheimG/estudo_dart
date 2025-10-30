0cc06a69239136a37c2ebca4c8e8711fbe171f33
GerheimG <g.gerheimferro@gmail.com>
2025-10-30 16:25:55 -0300

    atividade

--- atividades/atividade_3/exercicio_16.dart
+++ atividades/atividade_3/exercicio_16.dart
@@ -2,19 +2,113 @@
 // Dadas duas listas de inteiros, crie uma nova lista contendo
 // apenas os elementos que estão presentes em ambas as listas (a interseção).
 
+import 'dart:io';
+
 void main() {
-  List<int> lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-  List<int> lista2 = [2, 4, 6, 8, 10, 11, 12, 13];
 
-  List<int> resultado = [];
+  while (true) {
+    List<int> lista1 = [];
+    List<int> lista2 = [];
+    int? quantidade1;
+    int? quantidade2;
 
-  for (var n in lista1) {
-    for (var m in lista2) {
-      if (n == m) {
-        resultado.add(n);
+    while (quantidade1 == null) {
+      stdout.write('Quantos itens na primeira lista: ');
+      String? entradaQtnd1 = stdin.readLineSync();
+
+      if (entradaQtnd1 == null || entradaQtnd1.isEmpty) {
+        print('Entrada inválida');
+        continue;
+      }
+
+      quantidade1 = int.tryParse(entradaQtnd1);
+      if (quantidade1 == null || quantidade1 < 0) {
+        print('Insira uma quantidade válida');
+        quantidade1 = null;
       }
     }
+
+    for (int i = 0; i < quantidade1; i++) {
+      int? numero1;
+      while (numero1 == null) {
+        stdout.write('${i + 1}º Valor: ');
+        String? entradaValor = stdin.readLineSync();
+
+        if (entradaValor == null || entradaValor.isEmpty) {
+          print('Entrada inválida');
+          continue;
+        }
+
+        numero1 = int.tryParse(entradaValor);
+        if (numero1 == null) {
+          print('Entrada inválida');
+        }
+      }
+      lista1.add(numero1);
+    }
+
+    while (quantidade2 == null) {
+      stdout.write('Quantos itens na segunda lista: ');
+      String? entradaQtnd2 = stdin.readLineSync();
+
+      if (entradaQtnd2 == null || entradaQtnd2.isEmpty) {
+        print('Entrada inválida');
+        continue;
+      }
+
+      quantidade2 = int.tryParse(entradaQtnd2);
+      if (quantidade2 == null || quantidade2 < 0) {
+        print('Insira uma quantidade válida');
+        quantidade2 = null;
+      }
+    }
+    for (int i = 0; i < quantidade2; i++) {
+      int? numero2;
+      while (numero2 == null) {
+        stdout.write('${i + 1}º Valor: ');
+        String? entradaValor = stdin.readLineSync();
+
+        if (entradaValor == null || entradaValor.isEmpty) {
+          print('Entrada inválida');
+          continue;
+        }
+
+        numero2 = int.tryParse(entradaValor);
+        if (numero2 == null) {
+          print('Entrada inválida');
+        }
+      }
+      lista2.add(numero2);
+    }
+      List<int> resultado = [];
+
+      for (int n in lista1) {
+        for (int m in lista2) {
+          if (n == m) {
+            resultado.add(n);
+          }
+        }
+      }
+      
+      print(resultado);
+
+      while (true) {
+        stdout.write('Deseja continuar: (s/n)');
+        String? resposta = stdin.readLineSync();
+
+        if (resposta == null) continue;
+
+        resposta = resposta.toLowerCase();
+
+        if (resposta == 's') {
+          break;
+        } else if (resposta == 'n') {
+          print('Fechando');
+          return;
+        } else {
+          print('Resposta inválida');
+        }
+      }
   }
-  print(resultado);
 }
 
--- atividades/atividade_3/exercicio_5.dart
+++ atividades/atividade_3/exercicio_5.dart
@@ -3,16 +3,74 @@
 // A lista pode conter duplicatas, mas o segundo maior valor deve ser o valor distinto que é 
 //imediatamente menor que o maior valor.
 
+import 'dart:io';
+
 void main() {
-  List<int> numeros = [5, 8, 3, 10, 8, 12, 3, 15, 12, 20];
 
+  while (true) {
+    List<int> numeros = [];
+    int? quantidade;
 
-  List<int> distintos = numeros.toSet().toList();
+    while (quantidade == null) {
+      stdout.write('Quantos valores: ');
+      String? entradaValor = stdin.readLineSync();
 
-  distintos.sort();
+      if (entradaValor == null || entradaValor.isEmpty) {
+        print('Valor inválido');
+        continue;
+      }
 
-  int segundoMaior = distintos[distintos.length - 2];
+      quantidade = int.tryParse(entradaValor);
+      if (quantidade == null || quantidade < 0) {
+        print('Entrada inválida');
+        quantidade = null;
+      }
+    }
 
-  print('O segundo maior valor é: $segundoMaior');
+    for (int i = 0; i < quantidade; i++){
+      int? numero;
+      while (numero == null) {
+        stdout.write('${i + 1}º Valor (insira duplicatas)');
+        String? entradaValor = stdin.readLineSync();
+
+        if (entradaValor == null || entradaValor.isEmpty) {
+          print('Entrada inválida');
+          continue;
+        }
+        numero = int.tryParse(entradaValor);
+        if (numero == null) {
+          print('Entrada inválida');
+        }
+      }
+      numeros.add(numero);
+    }
+    // Remove os elementos duplicados da lista 'numeros' e cria uma nova lista apenas com valores distintos
+      List<int> distintos = numeros.toSet().toList();
+
+      distintos.sort();
+      if (distintos.length < 2) {
+        print('Não há segundo maior valor, pois todos os números são iguais ou há apenas um valor inserido.');
+      } else {
+        int segundoMaior = distintos[distintos.length - 2];
+        print('O segundo maior valor é: $segundoMaior');
+      }
+
+      while (true) {
+        stdout.write('Deseja continuar: (s/n)');
+        String? resposta = stdin.readLineSync();
+
+        if (resposta == null) continue;
+
+        resposta = resposta.toLowerCase();
+        if (resposta == 's') {
+          break;
+        } else if (resposta == 'n') {
+          print('Fechando');
+          return;
+        } else {
+          print('Resposta inválida');
+        }
+      }
+  }
 }
 
