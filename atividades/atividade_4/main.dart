40eebc807f5d51ddd90bdaab09d11841436c67d9
GerheimG <g.gerheimferro@gmail.com>
2025-11-11 16:05:56 -0300

    refatoração

--- atividades/atividade_4/main.dart
+++ atividades/atividade_4/main.dart
@@ -17,6 +17,7 @@ import 'dart:io';
 import 'menu.dart';
 import 'listar_contatos.dart';
 import 'formatar_nome.dart';
+import 'cadastro.dart';
 
 
 void main() {
@@ -27,74 +28,7 @@ void main() {
     int? opcao = mostrarMenu();
         switch (opcao) {
           case 1:
-            String? nomeContato;
-            String? telefoneContato;
-            String? emailContato;
-
-            while (nomeContato == null) {
-              stdout.write('Insira o nome do contato: ');
-              String? entradaNome = stdin.readLineSync();
-
-              if (entradaNome != null && entradaNome.trim().isNotEmpty) {
-                nomeContato = formatarNome(entradaNome);
-              } else {
-                print('Nome inválido.');
-                continue;
-              }
-
-              print('Caso não queria informar algum digite "0"');
-
-            while (emailContato == null) {
-                  stdout.write('Insira o email: ');
-                  String? entradaEmail = stdin.readLineSync();
-
-                  if (entradaEmail == '0') {
-                    print('Não informado');
-                    emailContato = 'Não informado';
-                    break;
-                  }
-                  if (entradaEmail == null || entradaEmail.isEmpty) {
-                    print('Insira um email válido.');
-                    continue;
-                  }
-                  if (entradaEmail.contains('@') && entradaEmail.endsWith('.com')) {
-                    emailContato = entradaEmail.trim();
-                  } else {
-                    print('Email inválido');
-                    continue;
-                  }
-                }
-              
-            while (telefoneContato == null) {
-                  stdout.write('Insira o telefone (sem DDD) xxxxx-xxxx: ');
-                  String? entradaTel = stdin.readLineSync();
-
-                  if (entradaTel == '0') {
-                    print('Não informado');
-                    telefoneContato = 'Não informado';
-                    break;
-                  }
-                  if (entradaTel == null || entradaTel.isEmpty) {
-                    print('Entrada inválida');
-                    continue;
-                  }
-                  if (entradaTel.length > 10 || entradaTel.length < 10) {
-                    print('Insira um número de telefone válido');
-                    continue;
-                  }
-                  if (entradaTel.contains('-')) {
-                    telefoneContato = entradaTel.trim();
-                  } else {
-                    print('Telefone inválido. Insira "-" ');
-                    continue;
-                  }
-                }
-              agenda.add({
-                'nome' : nomeContato,
-                'email' : emailContato,
-                'telefone' : telefoneContato
-              });
-            }
+            cadastrar(agenda);
             break;
           case 2:
               listarContatos(agenda);
@@ -118,9 +52,7 @@ void main() {
                 (contato) => contato['nome'].toLowerCase().contains(nomeBusca!.toLowerCase())
               ).toList();
               print('Resultado da Busca');
-              for (var n in filtro) {
-                print('Nome: ${n['nome']} | Email: ${n['email']} | Telefone: ${n['telefone']}');
-              }
+              listarContatos(filtro);
             }
             break;
           case 4:
